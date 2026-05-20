5563568d91198e9ff8a4c2c7e50ff1e10991d064
SpacePanda7077 <spacepanda7077@gmail.com>
2026-05-20 08:22:21 -0400

    changed vite config

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,8 +1,6 @@
 const isDev = import.meta.env.DEV;
 
-export const API_BASE_URL = isDev
-    ? "/api"
-    : "https://api.allorigins.win/get?url=http://92.205.187.214:8080";
+export const API_BASE_URL = isDev ? "/api" : "http://92.205.187.214:8080";
 
 export const WS_BASE_URL = isDev ? "" : "ws://92.205.187.214:8080";
 
