585852579378dabb7ef819a86089d5263434953e
SpacePanda7077 <spacepanda7077@gmail.com>
2026-05-20 08:19:43 -0400

    changed vite config

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,7 +2,7 @@ const isDev = import.meta.env.DEV;
 
 export const API_BASE_URL = isDev
     ? "/api"
-    : "https://corsproxy.io/?http://92.205.187.214:8080";
+    : "https://api.allorigins.win/get?url=http://92.205.187.214:8080";
 
 export const WS_BASE_URL = isDev ? "" : "ws://92.205.187.214:8080";
 
